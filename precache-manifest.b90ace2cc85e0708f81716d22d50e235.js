48ce842abf647398b08344b2d7fe6bbdfd213b26
FrapoDeveloper <fran_espino189@outlook.com>
2021-01-17 09:51:51 -0500

    Updates

---
+++ precache-manifest.b90ace2cc85e0708f81716d22d50e235.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85bfc21aebfdf64a24721a225e17b196",
+    "url": "/sgnk/index.html"
+  },
+  {
+    "revision": "971d109434aa5bcdb59c",
+    "url": "/sgnk/static/css/main.bca2165c.chunk.css"
+  },
+  {
+    "revision": "c1f8c73371aa9d60cb9f",
+    "url": "/sgnk/static/js/2.90af2135.chunk.js"
+  },
+  {
+    "revision": "0d10b9de47c8023f7fd9d830b32d5242",
+    "url": "/sgnk/static/js/2.90af2135.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "971d109434aa5bcdb59c",
+    "url": "/sgnk/static/js/main.997561c4.chunk.js"
+  },
+  {
+    "revision": "5dbbd1ae49bf8deabc37",
+    "url": "/sgnk/static/js/runtime-main.e8c89c3c.js"
+  },
+  {
+    "revision": "b37b05a713d8eebcbb475119e587859b",
+    "url": "/sgnk/static/media/github.b37b05a7.png"
+  },
+  {
+    "revision": "4a8c23476a7c20c5bee2a752a6f96e9e",
+    "url": "/sgnk/static/media/instagram.4a8c2347.png"
+  }
+]);
